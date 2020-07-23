58d33c9ac094f85e462c155395b3ea0c0484ba23
JeremyGlik <60774301+JeremyGlik@users.noreply.github.com>
2020-07-23 10:22:49 +0200

    Day 2 - Save 2

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -7,6 +7,7 @@ var mySwiper = new Swiper('.swiper-container', {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
   
     // Navigation arrows
